ada5a6485d3047501895f5c98d1fbc3472e95650
Dodocyborg <140459371+Dodocyborg@users.noreply.github.com>
2024-12-23 19:42:41 -0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,2 +1,7 @@
 // bot.js
 console.log("Dodo Bank Bot is active. Monitoring system...");
+<script src="Bot.js"></script>
+<script src="EmployeeCentral Banklogin.js"></script>
+<script src="Transaction.js"></script> <!-- Example JS for handling transactions -->
+</body>
+</html>
